dce21b43891cd20a9c1377e707438ec8dc16d4e0
Hamid Ftillou <ftillouhamid@gmail.com>
2018-06-14 16:30:24 +0000

    modification /c correction iconPath selon process.platform

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 const {app, BrowserWindow, Tray, Menu} = require('electron');
 const path=require('path');
-let  iconPath= path.join(__dirname,'assets/icons/win/unimatelLogo.ico');
 
+const pathIcone= (process.platform == "win32" )  ? 'assets/icons/win/unimatelLogo.ico' 
+      :(process.platform == "darwin" ) ? 'assets/icons/mac/unimatelLogo.icns' : 'assets/icons/png/unimatelLogo.png' 
+
+let  iconPath= path.join(__dirname,pathIcone);
 let appIcon = null;
 let top = null;
 let child = null; 
 
-
 app.on('ready', function() {
     top = new BrowserWindow({  icon: iconPath, kiosk: true, thickFrame :true, 
                                 backgroundColor: '#2e2c29'                            });
-    //top.loadURL('file://'+__dirname+'/index.html');
     top.loadFile('index.html');
+    
 
     child=new BrowserWindow({parent: top,  width:500, height:400, icon: iconPath});
-    //child.loadURL('file://'+__dirname+'/child.html');
     child.loadURL('http://192.168.1.8/estimation.html?x=0');
     child.setMenuBarVisibility(false);
     child.on('resize', function() { 
-       // console.log(child.width+ ','+child.height);
        console.log(child.getBounds());
-     //   document.getElementById('message').innerHTML =child.width+ ','+child.height;
         }
     );
     
